273a4f5a03a51b4625ff9bfe9be202ae58644ec4
psphmarina <north.pole@list.ru>
2019-06-13 11:54:04 +0400

    5

---
+++ AircrafBuildingPlant/AircraftPlantServiceImplementDataBase/Migrations/201906100852570_ThirdMigration1.cs
@@ -0,0 +1,16 @@
+namespace AircraftPlantServiceImplementDataBase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ThirdMigration1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
